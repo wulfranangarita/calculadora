6ec71b705dade4cc0619378c316c24013401fc0e
Wulfran Angarita <wulfran.angarita@amerikatis.com>
2019-04-13 09:51:06 -0500

    two way binding2

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component} from "@angular/core";
+import { componentFactoryName } from "@angular/compiler";
+
+@Component({
+    selector : "app",
+    template:require("./app.component.html")
+   })
+
+   export class AppComponent{
+       nombre=" "
+
+
+   }
